ddb66113e19ec9a657104cd47fc322166b814d88
interdimensionalpizza <michaellake05@gmail.com>
2023-06-09 13:40:19 -0700

    finished python version

--- python/char_count.py
+++ python/char_count.py
@@ -1,2 +1,17 @@
 def char_count(str):
-  # Your code here
+  str = [*str]
+  str = sorted(str)
+  str = ''.join(str)
+  answer = []
+  log = []
+  for elem in str:
+    if elem == ' ':
+      continue
+    elif elem not in log:
+      answer.append([elem, 1])
+      log.append(elem)
+    elif elem in log:
+      answer[log.index(elem)][1] += 1
+  answer.sort(key = lambda x : x[1], reverse=True)
+    
+  return answer
